f782b8319511d2ad44f02663cd7cf22f922edc34
DaniilMoi <moiseev@179.ru>
2021-11-10 13:23:30 +0300

    Make two tasks

---
+++ Lesson2/Task1/Program.cs
@@ -0,0 +1,35 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace Task1
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            Console.WriteLine("Program find minimum of three numbers\nEnter three numbers-");
+            Console.Write("First number:");
+            int a = int.Parse(Console.ReadLine());
+            Console.Write("Second number:");
+            int b = int.Parse(Console.ReadLine()); 
+            Console.Write("Third number:");
+            int c = int.Parse(Console.ReadLine());
+            Console.WriteLine("Minimum of three numbers is {0}",Min(a,b,c));
+        }
+
+        static int Min(int a, int b, int c)
+            => Min(a, b) == a ? Min(a, c) : Min(b, c); 
+
+        static int Min(int a, int b)
+            => (a < b) ? a : b;
+
+        static int MinOfThree(int a,int b, int c)
+        {
+            int min = a < b ? a : b;
+            return min < c ? min : c;
+        }
+    }
+}
---
+++ Lesson2/Task2/Program.cs
@@ -0,0 +1,29 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace Task2
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            Console.Write("Program counting the number of digits of a number.\nEnter numbers:");
+            int number = int.Parse(Console.ReadLine());
+            Console.WriteLine("Total digits is {0}", TotalNumberOfDigits(number));
+        }
+
+        static int TotalNumberOfDigits(int number)
+        {
+            int count = 1;
+            while (Math.Abs(number) > 9)
+            {
+                number = number / 10;
+                count++;
+            }
+            return count;
+        }
+    }
+}
