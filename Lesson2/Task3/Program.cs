aa05949631e792604fd33cbb3dc14c74952bd65c
DaniilMoi <moiseev@179.ru>
2021-11-10 19:13:53 +0300

    Done task 3 and 4

---
+++ Lesson2/Task3/Program.cs
@@ -0,0 +1,29 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace Task3
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            Console.WriteLine("Program calculate the sum of all odd positive numbers.\nEnter numbers, when need to stop enter zero:");
+            int number = 0;
+            int sum = 0;
+            do
+            {
+                number = int.Parse(Console.ReadLine());
+                sum += !IsEven(number) && IsPositive(number) ? number : 0;
+
+            } while (number != 0);
+            Console.WriteLine($"Sum is {sum}");
+        }
+
+        static bool IsEven(int number) => number % 2 == 0 ? true : false;
+
+        static bool IsPositive(int number) => number > 0 ? true : false;
+    }
+}
---
+++ Lesson2/Task4/Program.cs
@@ -0,0 +1,53 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace Task4
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            if (Authorization())
+            {
+                Console.WriteLine("Start Application...");
+                //Menu();
+            }
+            Console.WriteLine("Exit Application...");
+        }
+
+        static bool Authorization()
+        {
+            Console.WriteLine("Start Authorization...");
+            int attempts = 3;
+            do
+            {
+                Console.Write("Login:");
+                string login = Console.ReadLine();
+                Console.Write("Password:");
+                string password = Console.ReadLine();
+                if (IsCorrectData(login, password))
+                {
+                    Console.WriteLine("Authorization sucsess!");
+                    return true;
+                }
+                else
+                {
+                    Console.WriteLine("Wrong login or password!");
+                    Console.WriteLine("You have {0} attempts.", --attempts);
+                }
+            } while (attempts > 0);
+            Console.WriteLine("Authorization failed!");
+            return false;
+        }
+
+        static bool IsCorrectData(string login, string password)
+        {
+            string rightLogin = "root";
+            string rightPassword = "GeekBrains";
+            return rightLogin.Equals(login) && rightPassword.Equals(password);
+        }
+    }
+}
