3596d47ae833d2e6fbeae1ff3252cf738bcd48a0
DaniilMoi <moiseev@179.ru>
2021-11-29 19:48:00 +0300

    did tasks 1 and 2a

---
+++ Lesson6/Task1/Program.cs
@@ -0,0 +1,48 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace Task1
+{
+    /// <summary>
+    /// 1. Изменить программу вывода таблицы функции так, чтобы можно было передавать функции типа double (double, double). 
+    /// Продемонстрировать работу на функции с функцией a*x^2 и функцией a*sin(x).
+    /// </summary>
+    
+    // Описываем делегат. В делегате описывается сигнатура методов, на
+    // которые он сможет ссылаться в дальнейшем (хранить в себе)
+    public delegate double Fun(double a, double x);
+    class Program
+    {// Создаем метод, который принимает делегат
+        // На практике этот метод сможет принимать любой метод
+        // с такой же сигнатурой, как у делегата
+        public static void Table(Fun F, double a, double x, double b)
+        {
+            Console.WriteLine("----- A ----- X ----- Y -----");
+            while (a <= b)
+            {
+                double startX = x;
+                while (x <= b)
+                {
+                    Console.WriteLine("| {0,8:0.000} | {1,8:0.000} | {2,8:0.000} |", a, x, F(a, x));
+                    x += 1;
+                }
+                a++;
+                x = startX;
+            }
+            Console.WriteLine("---------------------");
+        }
+
+        static void Main()
+        {
+            Console.WriteLine("Таблица функции a*Sin(x):");
+            Table((a,x)=>a*Math.Sin(x),-2, -2, 2);
+
+            Console.WriteLine("Таблица функции a*x^2:");
+            Table(delegate (double a, double x) { return a*x * x; }, -2, -2, 2);
+
+        }
+    }
+}
---
+++ Lesson6/Task2/Program.cs
@@ -0,0 +1,140 @@
+﻿using System;
+using System.Collections.Generic;
+using System.IO;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace Task2
+{
+    /// <summary>
+    /// 2. Модифицировать программу нахождения минимума функции так, чтобы можно было передавать функцию в виде делегата.
+    /// а) Сделать меню с различными функциями и представить пользователю выбор, для какой функции и на каком отрезке находить минимум.
+    /// Использовать массив(или список) делегатов, в котором хранятся различные функции.
+    /// 
+    /// б) * Переделать функцию Load, чтобы она возвращала массив считанных значений.
+    /// Пусть она возвращает минимум через параметр(с использованием модификатора out).
+    /// </summary>
+    class Program
+    {
+        static public Fun[] functionsArr =
+        {
+            (x) => Math.Cos(x),
+            (x) => x* x + 3*x + 8,
+            (x) => 3*x* x*x + 5*x* x - 7,
+            (x) => -x + 2,
+            (x) => 1/x
+        };
+
+        private static double F(double x)
+        {
+            return x * x - 50 * x + 10;
+        }
+
+        public delegate double Fun(double x);
+        public static void SaveFunc(string fileName, Fun F, double a, double b, double h)
+        {
+            FileStream fs = new FileStream(fileName, FileMode.Create, FileAccess.Write);
+            BinaryWriter bw = new BinaryWriter(fs);
+            double x = a;
+            while (x <= b)
+            {
+                bw.Write(F(x));
+                x += h;// x=x+h;
+            }
+            bw.Close();
+            fs.Close();
+        }
+        public static double Load(string fileName)
+        {
+            FileStream fs = new FileStream(fileName, FileMode.Open, FileAccess.Read);
+            BinaryReader bw = new BinaryReader(fs);
+            double min = double.MaxValue;
+            double d;
+            for (int i = 0; i < fs.Length / sizeof(double); i++)
+            {
+                // Считываем значение и переходим к следующему
+                d = bw.ReadDouble();
+                if (d < min) min = d;
+            }
+            bw.Close();
+            fs.Close();
+            return min;
+        }
+        static void Main(string[] args)
+        {
+            while (true)
+            {
+                Fun UsingFunction = ChoseFunction();
+                if (UsingFunction == null)
+                {
+                    Console.WriteLine("Exit...");
+                    return;
+                }
+
+
+                Console.Write("Enter min x:");
+                int xMin = EnterNumber();
+                Console.Write("Enter max x:");
+                int xMax = EnterNumber();
+
+                SaveFunc("data.bin", UsingFunction, xMin, xMax, 0.5);
+                Console.WriteLine(Load("data.bin"));
+
+                Console.WriteLine("Press any key to continue...");
+                Console.ReadKey();
+            }
+        }
+
+        
+
+        public static int EnterNumber()
+        {
+            do
+            {
+                Console.Write("Enter number:");
+                bool flag = int.TryParse(Console.ReadLine(), out int number);
+                if (flag)
+                {
+                    return number;
+                }
+                else
+                {
+                    Console.WriteLine("Incorrect input!");
+                }
+            } while (true);
+        }
+
+        public static Fun ChoseFunction()
+        {
+            do
+            {
+                PrintMenu();
+                int funcNum = EnterNumber();
+                switch (funcNum)
+                {
+                    case (0):
+                        return null;
+                    default:
+                        if (funcNum > 0 && functionsArr.Length >= funcNum)
+                            return functionsArr[funcNum - 1];
+                        else
+                            Console.WriteLine("Enter another number");
+                        break;
+                }
+
+            } while (true);
+        }
+
+        static void PrintMenu()
+        {
+            Console.WriteLine("Functions");
+            Console.WriteLine("1)3*Cos(x) - 1");
+            Console.WriteLine("2)x ^ 2 + 3 * x + 8");
+            Console.WriteLine("3)3x^3 + 5x^2 - 7");
+            Console.WriteLine("4)-x +2");
+            Console.WriteLine("5)1/x");
+            Console.WriteLine("0)Exit");
+        }
+    }
+}
